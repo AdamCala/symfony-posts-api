6849325158b80b3513c090e279adcac8f04f3bd6
Adam Cala <adam.cala31@gmail.com>
2024-05-04 15:46:22 +0200

    /posts endpoint

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -6,9 +6,15 @@ use ApiPlatform\Metadata\ApiResource;
 use App\Repository\PostRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 
 #[ORM\Entity(repositoryClass: PostRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    operations: [
+        new Get(uriTemplate: '/posts/{id}'),
+        new GetCollection(uriTemplate: '/posts')
+    ])]
 class Post
 {
     #[ORM\Id]
